4bb73d1d94af64f7593f7c7dda7b1894b89cbaab
Samer Zagzoog <mrsamer.dev@gmail.com>
2020-07-03 17:21:01 +0300

    API Method Get/ Using Axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <h1>Hello from react</h1>
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,21 +1,18 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
-    <div className="card text-center">
+    <div className='card text-center'>
       <img
         src={avatar_url}
-        alt=""
-        className="round-img"
-        style={{ width: "60px" }}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
       />
       <h3>{login}</h3>
-
       <div>
-        <a href={html_url} className="btn btn-dark btn-sm my-1">
-          More
-        </a>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>More</a>
       </div>
     </div>
   );
@@ -25,4 +22,4 @@ UserItem.propTypes = {
   user: PropTypes.object.isRequired
 };
 
-export default UserItem;
+export default UserItem
